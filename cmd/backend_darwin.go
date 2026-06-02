bb493623e1c04e4011de35887a514258f3058126
Francois-Xavier 'FiX' KOWALSKI <fix.kowalski@gmail.com>
2026-06-03 00:35:11 +0200

    Add --keychain/-k flag on macOS to force login.keychain-db backend

--- cmd/backend_darwin.go
+++ cmd/backend_darwin.go
@@ -4,7 +4,17 @@ package cmd
 
 import "github.com/asnowfix/secret/backend"
 
+var forceKeychain bool
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&forceKeychain, "keychain", "k", false,
+		"force the Keychain backend (login.keychain-db via /usr/bin/security)")
+}
+
 func selectBackend() backend.Backend {
+	if forceKeychain {
+		return backend.NewKeychain()
+	}
 	pa := backend.NewPasswordsApp()
 	if pa.IsAvailable() == nil {
 		return pa
